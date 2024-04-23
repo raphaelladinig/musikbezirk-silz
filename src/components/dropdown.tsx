bdb1f5cfdf9657f6594226ed9e9ceeb360f1f0c7
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-04-23 21:17:58 +0200

    fix dropdown

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -7,21 +7,53 @@ export default function Dropdown(props: { title: string; subMenus: string[] }) {
 
   return (
     <div>
-      <button
-        onClick={toggle}
-        class="align-middle font-bold border-gray-300 border-2 px-2 hover:bg-gray-50"
-      >
-        {props.title}
-      </button>
-      {isOpen() && (
-        <ul class="absolute z-10 bg-gray-200 p-2 m-1 border border-white">
-          {props.subMenus.map((subMenu) => (
-            <li class="block text-start">
-              <a href={subMenu}>{subMenu}</a>
-            </li>
-          ))}
-        </ul>
-      )}
+      <div class="relative inline-block text-left">
+        <div>
+          <button
+            onClick={toggle}
+            type="button"
+            class="inline-flex w-full justify-center gap-x-1.5  bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            id="menu-button"
+            aria-expanded="true"
+            aria-haspopup="true"
+          >
+            {props.title}
+            <svg
+              class="-mr-1 h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        {isOpen() && (
+          <div
+            class="absolute right-0 z-10 mt-2 w-56 origin-top-right bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="menu-button"
+            tabindex="-1"
+          >
+            {props.subMenus.map((subMenu) => (
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                role="menuitem"
+                tabindex="-1"
+                id="menu-item-0"
+              >
+                {subMenu}
+              </a>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
