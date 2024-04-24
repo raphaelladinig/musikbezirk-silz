67087fef2c7b616c2332680089bb300018933838
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-04-24 09:27:43 +0200

    organize svgs

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,19 @@
 export default function Footer() {
   return (
-    <footer class="bg-black p-3 text-white">
+    <footer class="flex bg-black p-3 text-white gap-4">
       <a href="https://www.facebook.com/mbsilz/" class="hover:underline">
-        <div class="flex gap-3">
+        <div class="flex gap-2">
           <img src="../public/facebook.svg" class="size-5" />
           <p>Facebook</p>
+        </div>
+      </a>
+      <a
+        href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=alfred.lercher@gmail.com"
+        class="hover:underline"
+      >
+        <div class="flex gap-3">
+          <img src="../public/envelope-solid.svg" class="size-5" />
+          <p>GMail</p>
         </div>
       </a>
     </footer>
