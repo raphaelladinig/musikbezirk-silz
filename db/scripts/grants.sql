18b1348e253992fdfd16b2487e41d25e5c83f79b
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-08 14:33:43 +0200

    format

--- db/scripts/grants.sql
+++ db/scripts/grants.sql
@@ -1,3 +1,17 @@
-GRANT SELECT, INSERT, DELETE ON TABLE pages TO postgres;
-GRANT SELECT, INSERT, DELETE ON TABLE posts TO postgres;
-GRANT SELECT, INSERT, DELETE ON TABLE post_contents TO postgres;
+GRANT
+SELECT
+,
+  INSERT,
+  DELETE ON TABLE pages TO postgres;
+
+GRANT
+SELECT
+,
+  INSERT,
+  DELETE ON TABLE posts TO postgres;
+
+GRANT
+SELECT
+,
+  INSERT,
+  DELETE ON TABLE post_contents TO postgres;
