53719496bb722134327a287cd508496c93e3698f
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-22 09:05:30 +0200

    small bugfix

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const queryClient = postgres(process.env.DB_URL);
+const queryClient = postgres(process.env.DB_URL || "");
 const db = drizzle(queryClient);
 
 console.log("openend db connection");
